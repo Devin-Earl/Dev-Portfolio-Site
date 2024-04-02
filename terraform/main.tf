939163b75024e9d6bf12c1d038a02e27a9bd4a94
[Devin Earl] <[Devin@devinsight.co]>
2024-04-02 11:55:03 -0500

    moved state to spacelift

--- terraform/main.tf
+++ terraform/main.tf
@@ -30,9 +30,12 @@ terraform {
 }
 
 provider "azurerm" {
-  features {
+   features {}
 
-  }
+  client_id       = var.ARM_CLIENT_ID
+  client_secret   = var.ARM_CLIENT_SECRET
+  tenant_id       = var.ARM_TENANT_ID
+  subscription_id = var.ARM_SUBSCRIPTION_ID
 }
 resource "azurerm_resource_group" "example" {
   name     = var.acr_rg_name

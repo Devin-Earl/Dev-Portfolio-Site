f6d39dca8b4c8f3929831e749c97ad91e269b2a5
[Devin Earl] <[Devin@devinsight.co]>
2024-04-02 12:05:25 -0500

    moved state to spacelift

--- terraform/main.tf
+++ terraform/main.tf
@@ -21,7 +21,7 @@ terraform {
 
     workspaces {
 
-      name = "portfolio-site-sbx" 
+      name = "test-stack" 
 
     }
 
@@ -32,10 +32,10 @@ terraform {
 provider "azurerm" {
    features {}
 
-  client_id       = var.ARM_CLIENT_ID
-  client_secret   = var.ARM_CLIENT_SECRET
-  tenant_id       = var.ARM_TENANT_ID
-  subscription_id = var.ARM_SUBSCRIPTION_ID
+#   client_id       = var.ARM_CLIENT_ID
+#   client_secret   = var.ARM_CLIENT_SECRET
+#   tenant_id       = var.ARM_TENANT_ID
+#   subscription_id = var.ARM_SUBSCRIPTION_ID
 }
 resource "azurerm_resource_group" "example" {
   name     = var.acr_rg_name
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -60,19 +60,19 @@ variable "spacelift_key_secret" {
     type = string
   
 }
-variable "ARM_CLIENT_ID" {
-    type = string
+# variable "ARM_CLIENT_ID" {
+#     type = string
   
-}
-variable "ARM_CLIENT_SECRET" {
-    type = string
+# }
+# variable "ARM_CLIENT_SECRET" {
+#     type = string
   
-}
-variable "ARM_SUBSCRIPTION_ID" {
-    type = string
+# }
+# variable "ARM_SUBSCRIPTION_ID" {
+#     type = string
   
-}
-variable "ARM_TENANT_ID" {
-    type = string
+# }
+# variable "ARM_TENANT_ID" {
+#     type = string
   
-}
+# }

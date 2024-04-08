102c89f4bd70b6f23e8aa1ac2238b68eac40ed07
[Devin Earl] <[Devin@devinsight.co]>
2024-04-07 19:00:34 -0500

    azure web app config test

--- terraform/main.tf
+++ terraform/main.tf
@@ -73,7 +73,7 @@ resource "azurerm_linux_web_app" "main" {
     always_on        = true
      application_stack {
     docker_image_name= "dev-portfolio-2:latest"
-    docker_registry_url= azurerm_container_registry.acr.acr_login_server_url
+    docker_registry_url= azurerm_container_registry.acr.login_server
     docker_registry_username= azurerm_container_registry.acr.admin_username
     docker_registry_password= azurerm_container_registry.acr.admin_password
   }
